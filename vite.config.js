f1a4e261deb90339ee49e7843fb775a295d9c4d2
bigi <0311bigi@gmail.com>
2026-07-05 23:47:21 +0800

    Clean gh-pages

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 請把下面這行的 my-ledger 換成您在 GitHub 上的「Repository (儲存庫) 名稱」
-  base: '/Play_money/',
-})
